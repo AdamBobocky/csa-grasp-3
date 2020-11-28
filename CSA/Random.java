919729edef6441bbece9535b7221e778c40c069d
git <vojtawilson@gmail.com>
2020-11-28 18:57:35 +0100

    classes created

---
+++ CSA/Random.java
@@ -0,0 +1,23 @@
+package CSA;
+import java.lang.Math;
+
+public class Random {
+
+    public int GetRandomNumber() {
+
+    }
+
+    public String GetRandomName() {
+
+        String[] fName = {"Jeff", "Bill", "Mark", "Warren", "Larry", "Steve", "Elon", "Sergey", "Alice"};
+        String[] lName = {"Bezos", "Gates", "Zuckerberg", "Buffet", "Ellison", "Ballmer", "Musk", "Brin", "Walton"};
+
+
+
+    }
+
+    public String GetRandomBook() {
+
+    }
+
+}
