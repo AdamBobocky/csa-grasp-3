8f7261575d814580fdc7ef15016f6d09e6df2c39
Adam Bobocky <adambobocky@gmail.com>
2020-11-28 19:00:33 +0100

    New readme and new classes

--- Library.java
+++ Library.java
@@ -1,6 +1,20 @@
 import CSA.Book;
 
 public class Library {
-    public static void main(String[] args) {
-    }
+  public Book[] Books;
+  // public Reader[] Readers;
+  int Date;
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to this 'Not-good Not-bad Librarian Software©'");
+  }
+  public void ChangeDate(int NewDate) {
+    this.Date = NewDate;
+
+    System.out.println("Date is now: " + this.Date);
+  }
+  @Override
+  public String toString() {
+    return "";
+  }
 }
