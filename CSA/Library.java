19cf4cd2f6e0d163c99c3a727c6a96106f0a5510
Adam Bobocky <adambobocky@gmail.com>
2020-11-28 19:24:05 +0100

    First compilable and working version, the so called 0.1

--- CSA/Library.java
+++ CSA/Library.java
@@ -3,11 +3,14 @@ package CSA;
 import java.util.ArrayList;
 
 public class Library {
+  public LibraryRandom random;
   public ArrayList<Book> Books;
   public ArrayList<Reader> Readers;
   int Date;
 
   public Library() {
+    random = new LibraryRandom();
+
     System.out.println("Welcome to this 'Not-good Not-bad Librarian Software©'");
   }
   public void ChangeDate(int NewDate) {
--- Runner.java
+++ Runner.java
@@ -2,9 +2,9 @@ import CSA.*;
 
 public class Runner {
   public static void main(String[] args) {
-    System.out.println("Random reader name for fun and giggles: ");
-
     Library library = new Library();
+
+    System.out.println("Random reader name for fun and giggles: " + library.random.randomName());
 
     System.out.println(library.toString());
   }
