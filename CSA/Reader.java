ecd3788fbe7d62df4a5207741279144bf01afe19
git <vojtawilson@gmail.com>
2020-11-28 19:34:44 +0100

    Reader class progress

--- CSA/Reader.java
+++ CSA/Reader.java
@@ -2,4 +2,24 @@ package CSA;
 
 public class Reader {
 
+    private String name;
+
+    public Reader(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return this.name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    } 
+
 }
